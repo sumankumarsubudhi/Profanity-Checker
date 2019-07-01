6736d4ecefbe8cd88122caf04c143cd823e2e7b4
Suman Kumar Subudhi <sonu61866@gmail.com>
2019-07-01 23:44:04 +0530

    Add files via upload

---
+++ profanity_checker.py
@@ -0,0 +1,19 @@
+import urllib
+
+def file_read():
+    with open('/Users/sumankumarsubudhi/Desktop/Udacity/Python Foundation/movie_quotes.txt') as file:
+        file_text = file.read()
+        profanity_checker(file_text)
+
+def profanity_checker(some_text):
+    website = urllib.urlopen('http://www.wdylike.appspot.com/?q='+some_text)
+    profanity_value = website.read()
+    website.close()
+    if "true" in profanity_value:
+        print("Profanity Alert!!")
+    elif "false" in profanity_value:
+        print("Congrats!! The file don't contain any curse words.")
+    else:
+        print('There was some problem in loading the file.')
+
+file_read()
